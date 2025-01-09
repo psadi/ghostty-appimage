4850493eece9652042b24446ea44f0c8d36bc925
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-01-09 17:54:53 -0400

    declare `ARCH` first

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+export ARCH="$(uname -m)"
 GHOSTTY_VERSION="1.0.1"
 TMP_DIR="/tmp/ghostty-build"
 APP_DIR="${TMP_DIR}/ghostty.AppDir"
@@ -96,4 +97,4 @@ EOF
 cd "${TMP_DIR}"
 
 # create app image
-ARCH="$(uname -m)" appimagetool -u "${UPINFO}" "${APP_DIR}"
+appimagetool -u "${UPINFO}" "${APP_DIR}"
