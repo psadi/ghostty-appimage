d83b924f9ba27df5ef1710dcca1f03a5e51aeb71
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-03-13 02:07:12 -0400

    fix leftover `--runtime-file` flag

--- build.sh
+++ build.sh
@@ -103,4 +103,4 @@ cd "${TMP_DIR}"
 URUNTIME_PRELOAD=1 appimagetool --comp zstd \
 	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
 	--mksquashfs-opt -b --mksquashfs-opt 1M \
-	-u "${UPINFO}" "${APP_DIR}" --runtime-file /usr/local/bin/uruntime
+	-u "${UPINFO}" "${APP_DIR}"
